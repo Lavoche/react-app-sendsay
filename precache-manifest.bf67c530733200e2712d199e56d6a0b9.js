13a84fb3442ea5472f7ee5fd7865cc5527a48df5
lavoche <alexlavoche@gmail.com>
2020-06-22 15:31:40 +0300

    Updates

---
+++ precache-manifest.bf67c530733200e2712d199e56d6a0b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4252d427be1eedd08199ff8c7be26299",
+    "url": "/react-app-sendsay/index.html"
+  },
+  {
+    "revision": "5c130604eed8035597c4",
+    "url": "/react-app-sendsay/static/css/main.50fa76cd.chunk.css"
+  },
+  {
+    "revision": "ef3df84fb7c6a542d88b",
+    "url": "/react-app-sendsay/static/js/2.2989a64e.chunk.js"
+  },
+  {
+    "revision": "f9f833062c4f055b0909f70c155244ba",
+    "url": "/react-app-sendsay/static/js/2.2989a64e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c130604eed8035597c4",
+    "url": "/react-app-sendsay/static/js/main.f72a76b6.chunk.js"
+  },
+  {
+    "revision": "a93dc206e753da8d840d",
+    "url": "/react-app-sendsay/static/js/runtime-main.758f3555.js"
+  }
+]);
